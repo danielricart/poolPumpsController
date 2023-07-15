d1cf90b31372b3dd9c1770e9155c3556a3896b43
Daniel Ricart <danielricart@users.noreply.github.com>
2023-07-15 17:40:41 +0200

    added some status output.

--- waterLevelChlorineSide/mainCore-M7/mainCore-M7.ino
+++ waterLevelChlorineSide/mainCore-M7/mainCore-M7.ino
@@ -1,13 +1,24 @@
 #ifdef CORE_CM4
 #error "This Code is for CORE_CM7 only"
 #endif
+
+#include <avdweb_VirtualDelay.h>
+VirtualDelay updateSerialOutput;
+
+
 void setup() {
   // put your setup code here, to run once:
   bootM4();
+  updateSerialOutput.start(1000);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(LEDR, HIGH);
   digitalWrite(LEDG, HIGH);
+
+  if (updateSerialOutput.elapsed()) {
+    Serial.println("Hello from Main core...");
+    updateSerialOutput.start(1000);
+  }
 }
