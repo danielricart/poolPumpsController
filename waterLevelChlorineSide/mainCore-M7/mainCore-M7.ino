38246288e30a279d9a8f3f9ed903ff2f35b16843
Daniel Ricart <danielricart@users.noreply.github.com>
2023-07-15 21:26:50 +0200

    Adding Modbus support
    This PR enabled ModBus routines to PLCs.

--- waterLevelChlorineSide/mainCore-M7/mainCore-M7.ino
+++ waterLevelChlorineSide/mainCore-M7/mainCore-M7.ino
@@ -5,6 +5,24 @@
 #include <avdweb_VirtualDelay.h>
 #include "RPC.h"
 
+#include <SPI.h>
+#include <Ethernet.h>
+
+#include <ArduinoRS485.h>
+#include <ArduinoModbus.h>
+
+
+#define LED_CONN_ERROR LEDR
+#define LED_CONN_OK LEDG
+
+#define ADDR_REMOTE_MOTOR 0x00
+#define ADDR_REMOTE_LED_WATER_LEVEL 0x01
+#define ADDR_REMOTE_LED_CHLORINE 0x02
+IPAddress ip(192, 168, 1, 177);  //Local end
+
+EthernetClient ethClient;
+ModbusTCPClient modbusTCPClient(ethClient);
+IPAddress server(192, 168, 1, 10);  // remote end
 
 VirtualDelay updateSerialOutput;
 
@@ -12,7 +30,10 @@ bool waterLevel = false;
 bool chlorineStatus = false;
 bool motorOn = false;
 bool remoteMotorStatus = false;
-bool overrideBehaviour = false; 
+bool overrideBehaviour = false;
+
+bool modbusStatus = false;
+bool remoteComms = false;
 
 void setVariables(bool newWaterLevel, bool newChlorineStatus, bool newMotorOn, bool newOverride) {
   waterLevel = newWaterLevel;
@@ -33,18 +54,61 @@ void setup() {
   RPC.bind("setVariables", setVariables);
   RPC.bind("getRemoteMotorStatus", getRemoteMotorStatus);
   delay(200);
+
+  Ethernet.begin(ip);
+  if (Ethernet.linkStatus() == LinkOFF) {
+    Serial.println("Ethernet cable is not connected.");
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
-  
+  if (!modbusTCPClient.connected()) {
+    // client not connected, start the Modbus TCP client
+    Serial.println("Attempting to connect to Modbus TCP server");
+
+    if (!modbusTCPClient.begin(server, 502)) {
+      Serial.println("Modbus TCP Client failed to connect!");
+      modbusStatus = false;
+    } else {
+      Serial.println("Modbus TCP Client connected");
+      modbusStatus = true;
+    }
+  } else {  // client connected
+    modbusStatus = modbusTCPClient.connected();
+    remoteComms = true; //assuming everything will be ok. 
+    int remoteInput1 = modbusTCPClient.coilRead(0x00);
+    if (remoteInput1 < 0) {
+      Serial.print("Failed to read input! ");
+      Serial.println(modbusTCPClient.lastError());
+      remoteComms = false;
+    } else {
+      remoteMotorStatus = remoteInput1;
+    }
+
+    if (!modbusTCPClient.coilWrite(ADDR_REMOTE_MOTOR, turnMotorOn)) {
+      Serial.print("Failed to send turnMotorOn! ");
+      Serial.println(modbusTCPClient.lastError());
+      remoteComms = false;
+    }
+    if (!modbusTCPClient.coilWrite(ADDR_REMOTE_LED_WATER_LEVEL, waterLevel)) {
+      Serial.print("Failed to send waterLevel! ");
+      Serial.println(modbusTCPClient.lastError());
+      remoteComms = false;
+    }
+    if (!modbusTCPClient.coilWrite(ADDR_REMOTE_LED_CHLORINE, chlorineStatus)) {
+      Serial.print("Failed to send chlorineStatus! ");
+      Serial.println(modbusTCPClient.lastError());
+      remoteComms = false;
+    }
+  }
+
 
   //TODO: Fetch this from remote end.
-  remoteMotorStatus = motorOn;
-  
-  digitalWrite(LEDR, HIGH);
-  digitalWrite(LEDG, HIGH);
+  //remoteMotorStatus = motorOn;
+
+  digitalWrite(LEDR, !modbusStatus || !remoteComms);
+  digitalWrite(LEDG, modbusStatus || remoteComms);
 
 
   if (updateSerialOutput.elapsed()) {
